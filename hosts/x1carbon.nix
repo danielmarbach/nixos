1940c5a89269f809f8c5840ff89c12d26fe4b358
Daniel Marbach <daniel.marbach@openplace.net>
2023-05-28 21:55:28 +0200

    Update x1carbon.nix

--- hosts/x1carbon.nix
+++ hosts/x1carbon.nix
@@ -18,14 +18,9 @@
     kernelPackages = pkgs.linuxPackages_latest;
 
     initrd = {
-      availableKernelModules = [ "xhci_pci" "nvme" "usb_storage" "sd_mod" ];
+      availableKernelModules = [ "xhci_pci" "thunderbolt" "nvme" "usb_storage" "usbhid" "sr_mod" ];
       kernelModules = [ "dm-snapshot" ];
     };
-
-    kernelModules = [ "kvm-intel" ];
-
-    kernelParams =
-      [ "intel_pstate=passive" "i915.enable_fbc=1" "i915.enable_psr=2" ];
 
     loader = {
       grub = {
@@ -40,7 +35,23 @@
   };
 
   networking = {
-
     hostName = "x1carbon";
   };
+  
+  fileSystems."/" =
+  { device = "/dev/disk/by-uuid/be3e77ef-fa9a-46a3-a39a-f30aed6276d5";
+    fsType = "ext4";
+  };
+
+  boot.initrd.luks.devices."luks-d3d1c3d7-b8aa-454e-b016-39206f7a6580".device = "/dev/disk/by-uuid/d3d1c3d7-b8aa-454e-b016-39206f7a6580";
+
+  fileSystems."/boot/efi" =
+  { device = "/dev/disk/by-uuid/8E25-2DA0";
+    fsType = "vfat";
+  };
+    
+  powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
+  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  # high-resolution display
+  hardware.video.hidpi.enable = lib.mkDefault true;
 }
