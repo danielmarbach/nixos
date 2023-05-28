b8ee928be3379e5bd00aeb684699d2520972864b
Daniel Marbach <daniel.marbach@openplace.net>
2023-05-28 23:44:44 +0200

    Update default.nix

--- desktop/default.nix
+++ desktop/default.nix
@@ -31,6 +31,21 @@
       xdgOpenUsePortal = true;
     };
   };
+ 
+ services = {
+  xserver = {
+    displayManager = {
+      gdm = {
+        enable = true;
+      };
+    };
+    desktopManager = {
+      gnome = {
+        enable = true;
+      };
+    };
+  };
+ };
 
   hardware = {
     opengl = {
